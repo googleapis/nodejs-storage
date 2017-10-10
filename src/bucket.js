8563ee1637224bbc439a91a9fc3d4582b52792f5
Jason Dobry <jmdobry@users.noreply.github.com>
2017-10-10 13:49:48 -0700

    Fix wrong sample file reference

--- src/bucket.js
+++ src/bucket.js
@@ -1483,7 +1483,7 @@ Bucket.prototype.getMetadata = function(options, callback) {
  *   var apiResponse = data[1];
  * });
  *
- * @example <caption>include:samples/buckets.js</caption>
+ * @example <caption>include:samples/notifications.js</caption>
  * region_tag:storage_list_notifications
  * Another example:
  */
