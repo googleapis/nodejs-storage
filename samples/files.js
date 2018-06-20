1a75805d372967fc60f22380f148656410b32328
Jeffrey Posnick <jeffy@google.com>
2018-06-20 09:54:40 -0400

    More linting.

--- samples/files.js
+++ samples/files.js
@@ -132,12 +132,12 @@ function uploadFile(bucketName, filename) {
   storage
     .bucket(bucketName)
     .upload(filename, {
-       // Support for HTTP requests made with `Accept-Encoding: gzip`
+      // Support for HTTP requests made with `Accept-Encoding: gzip`
       gzip: true,
       metadata: {
-         // Enable long-lived HTTP caching headers.
-         // Use only if the contents of the file will never change.	
-         // (If the contents will change, use cacheControl: 'no-cache')
+        // Enable long-lived HTTP caching headers
+        // Use only if the contents of the file will never change
+        // (If the contents will change, use cacheControl: 'no-cache')
         cacheControl: 'public, max-age=31536000',
       },
     })
