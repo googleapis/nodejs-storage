cbf299020b5500eac7f7a62cf61a625e0d6e49fb
Frank Natividad <franknatividad@google.com>
2018-10-03 13:39:52 -0700

    Fix lint issue and merge issue

--- samples/files.js
+++ samples/files.js
@@ -226,41 +226,33 @@ async function getMetadata(bucketName, filename) {
   const [metadata] = await storage
     .bucket(bucketName)
     .file(filename)
-    .getMetadata()
-    .then(results => {
-      const metadata = results[0];
+    .getMetadata();
 
-      console.log(`File: ${metadata.name}`);
-      console.log(`Bucket: ${metadata.bucket}`);
-      console.log(`Storage class: ${metadata.storageClass}`);
-      console.log(`Self link: ${metadata.selfLink}`);
-      console.log(`ID: ${metadata.id}`);
-      console.log(`Size: ${metadata.size}`);
-      console.log(`Updated: ${metadata.updated}`);
-      console.log(`Generation: ${metadata.generation}`);
-      console.log(`Metageneration: ${metadata.metageneration}`);
-      console.log(`Etag: ${metadata.etag}`);
-      console.log(`Owner: ${metadata.owner}`);
-      console.log(`Component count: ${metadata.component_count}`);
-      console.log(`Crc32c: ${metadata.crc32c}`);
-      console.log(`md5Hash: ${metadata.md5Hash}`);
-      console.log(`Cache-control: ${metadata.cacheControl}`);
-      console.log(`Content-type: ${metadata.contentType}`);
-      console.log(`Content-disposition: ${metadata.contentDisposition}`);
-      console.log(`Content-encoding: ${metadata.contentEncoding}`);
-      console.log(`Content-language: ${metadata.contentLanguage}`);
-      console.log(`Media link: ${metadata.mediaLink}`);
-      console.log(`KMS Key Name: ${metadata.kmsKeyName}`);
-      console.log(`Temporary Hold: ${metadata.temporaryHold}`);
-      console.log(`Event-based hold: ${metadata.eventBasedHold}`);
-      console.log(
-        `Effective Expiration Time: ${metadata.effectiveExpirationTime}`
-      );
-      console.log(`Metadata: ${metadata.metadata}`);
-    })
-    .catch(err => {
-      console.error('ERROR:', err);
-    });
+  console.log(`File: ${metadata.name}`);
+  console.log(`Bucket: ${metadata.bucket}`);
+  console.log(`Storage class: ${metadata.storageClass}`);
+  console.log(`Self link: ${metadata.selfLink}`);
+  console.log(`ID: ${metadata.id}`);
+  console.log(`Size: ${metadata.size}`);
+  console.log(`Updated: ${metadata.updated}`);
+  console.log(`Generation: ${metadata.generation}`);
+  console.log(`Metageneration: ${metadata.metageneration}`);
+  console.log(`Etag: ${metadata.etag}`);
+  console.log(`Owner: ${metadata.owner}`);
+  console.log(`Component count: ${metadata.component_count}`);
+  console.log(`Crc32c: ${metadata.crc32c}`);
+  console.log(`md5Hash: ${metadata.md5Hash}`);
+  console.log(`Cache-control: ${metadata.cacheControl}`);
+  console.log(`Content-type: ${metadata.contentType}`);
+  console.log(`Content-disposition: ${metadata.contentDisposition}`);
+  console.log(`Content-encoding: ${metadata.contentEncoding}`);
+  console.log(`Content-language: ${metadata.contentLanguage}`);
+  console.log(`Media link: ${metadata.mediaLink}`);
+  console.log(`KMS Key Name: ${metadata.kmsKeyName}`);
+  console.log(`Temporary Hold: ${metadata.temporaryHold}`);
+  console.log(`Event-based hold: ${metadata.eventBasedHold}`);
+  console.log(`Effective Expiration Time: ${metadata.effectiveExpirationTime}`);
+  console.log(`Metadata: ${metadata.metadata}`);
   // [END storage_get_metadata]
 }
 
