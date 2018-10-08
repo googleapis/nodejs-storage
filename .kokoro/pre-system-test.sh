176a36000252a5fede0ec245611db9229597a3e0
Jonathan Lui <jonathanlui@google.com>
2018-10-08 14:11:14 -0700

    verify environment has service account

--- .kokoro/pre-system-test.sh
+++ .kokoro/pre-system-test.sh
@@ -14,4 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+gcloud config config-helper --format json | grep 'account'
+
 . .kokoro/setup-vars.sh
