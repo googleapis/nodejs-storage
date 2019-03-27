c97c2ba3d9ebb031b43e37819ed7ae8bd83f505f
Jonathan Lui <jonathanlui@google.com>
2019-03-27 12:16:19 -0700

    gts fix

--- src/file.ts
+++ src/file.ts
@@ -1341,7 +1341,8 @@ class File extends ServiceObject<File> {
    * @typedef {object} CreateResumableUploadOptions
    * @property {string} [configPath] Where the `gcs-resumable-upload`
    *     configuration file should be stored on your system. This maps to the
-   *     [configstore option by the same name](https://github.com/yeoman/configstore/tree/0df1ec950d952b1f0dfb39ce22af8e505dffc71a#configpath).
+   *     [configstore option by the same
+   * name](https://github.com/yeoman/configstore/tree/0df1ec950d952b1f0dfb39ce22af8e505dffc71a#configpath).
    * @property {object} [metadata] Metadata to set on the file.
    * @property {string} [origin] Origin header to set for the upload.
    * @property {string} [predefinedAcl] Apply a predefined set of access
@@ -1443,9 +1444,9 @@ class File extends ServiceObject<File> {
   /**
    * @typedef {object} CreateWriteStreamOptions Configuration options for File#createWriteStream().
    * @property {string} [configPath] **This only applies to resumable
-   *     uploads.** Where the `gcs-resumable-upload` configuration file should be
-   *     stored on your system. This maps to the
-   *     [configstore option by the same name](https://github.com/yeoman/configstore/tree/0df1ec950d952b1f0dfb39ce22af8e505dffc71a#configpath).
+   *     uploads.** Where the `gcs-resumable-upload` configuration file should
+   * be stored on your system. This maps to the [configstore option by the same
+   * name](https://github.com/yeoman/configstore/tree/0df1ec950d952b1f0dfb39ce22af8e505dffc71a#configpath).
    * @property {string} [contentType] Alias for
    *     `options.metadata.contentType`. If set to `auto`, the file name is used
    *     to determine the contentType.
