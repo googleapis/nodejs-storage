a9aaac15f43a5712ca1cae0d8f037400feaa5f15
Jonathan Lui <jonathanlui@google.com>
2019-07-08 14:00:36 -0700

    npm run fix

--- test/index.ts
+++ test/index.ts
@@ -274,10 +274,7 @@ describe('Storage', () => {
           assert.strictEqual(secret, response.secret);
           assert(hmacKey instanceof HmacKey);
           assert.strictEqual(hmacKey.metadata, metadataResponse);
-          assert.strictEqual(
-            hmacKey.accessId,
-            metadataResponse.accessId
-          );
+          assert.strictEqual(hmacKey.accessId, metadataResponse.accessId);
           done();
         }
       );
