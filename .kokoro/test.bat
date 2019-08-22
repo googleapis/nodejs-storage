d4167b6830f9d38c62570e2de483f11367187f40
Benjamin Coe <bencoe@google.com>
2019-08-22 16:46:44 -0700

    build: demonstrate npm bug

--- .kokoro/test.bat
+++ .kokoro/test.bat
@@ -22,9 +22,9 @@ cd ..
 @rem Node v8.9.1 to install npm@latest, it then uses this version of npm to
 @rem install npm for v10.15.3.
 call nvm use v8.9.1 || goto :error
-call node C:\Users\kbuilder\AppData\Roaming\nvm-ps\versions\v8.9.1\node_modules\npm-bootstrap\bin\npm-cli.js i npm -g || goto :error
+call node C:\Users\kbuilder\AppData\Roaming\nvm-ps\versions\v8.9.1\node_modules\npm-bootstrap\bin\npm-cli.js i npm@6.10.3 -g || goto :error
 call nvm use v10.15.3 || goto :error
-call node C:\Users\kbuilder\AppData\Roaming\nvm-ps\versions\v8.9.1\node_modules\npm\bin\npm-cli.js i npm -g || goto :error
+call node C:\Users\kbuilder\AppData\Roaming\nvm-ps\versions\v8.9.1\node_modules\npm\bin\npm-cli.js i npm@6.10.3 -g || goto :error
 
 call npm install || goto :error
 call npm run test || goto :error
