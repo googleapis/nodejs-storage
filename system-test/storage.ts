84b33f4cfa3c7f5724b0a12b82b29b422e20c263
Stephen <stephenplusplus@users.noreply.github.com>
2019-11-26 15:19:38 -0500

    is it a project ID thing?

--- system-test/storage.ts
+++ system-test/storage.ts
@@ -174,11 +174,14 @@ describe('storage', () => {
     let storageWithoutAuth: Storage;
 
     let GOOGLE_APPLICATION_CREDENTIALS: string | undefined;
+    let GOOGLE_CLOUD_PROJECT: string | undefined;
 
     before(done => {
       // CI authentication is done with ADC. Cache it here, restore it `after`
       GOOGLE_APPLICATION_CREDENTIALS =
         process.env.GOOGLE_APPLICATION_CREDENTIALS;
+      GOOGLE_CLOUD_PROJECT =
+        process.env.GOOGLE_CLOUD_PROJECT;
 
       privateBucket = bucket; // `bucket` was created in the global `before`
       privateFile = privateBucket.file('file-name');
@@ -187,6 +190,7 @@ describe('storage', () => {
 
     beforeEach(() => {
       delete process.env.GOOGLE_APPLICATION_CREDENTIALS;
+      delete process.env.GOOGLE_CLOUD_PROJECT;
       delete require.cache[require.resolve('../src')];
 
       const {Storage} = require('../src');
@@ -195,6 +199,7 @@ describe('storage', () => {
 
     after(() => {
       process.env.GOOGLE_APPLICATION_CREDENTIALS = GOOGLE_APPLICATION_CREDENTIALS;
+      process.env.GOOGLE_CLOUD_PROJECT = GOOGLE_APPLICATION_CREDENTIALS;
     });
 
     describe('public data', () => {
