cf7239fe420c7d46f4ed17a65454fb5c94545c1a
Sameena Shaffeeullah <shaffeeullah@google.com>
2020-10-28 17:15:20 +0000

    moved test

--- system-test/storage.ts
+++ system-test/storage.ts
@@ -3358,19 +3358,6 @@ describe('storage', () => {
       assert.strictEqual(body, localFile.toString());
     });
 
-    it('should create a signed read url with usableFrom', async () => {
-      const [signedReadUrl] = await file.getSignedUrl({
-        version: 'v4',
-        action: 'read',
-        usableFrom: Date.now() - 5000,
-        expires: Date.now() + 5000,
-      });
-
-      const res = await fetch(signedReadUrl);
-      const body = await res.text();
-      assert.strictEqual(body, localFile.toString());
-    });
-
     it('should work with multi-valued extension headers', async () => {
       const HEADERS = {
         'x-goog-custom-header': ['value1', 'value2'],
@@ -3447,6 +3434,19 @@ describe('storage', () => {
       const [signedReadUrl] = await file.getSignedUrl({
         version: 'v4',
         action: 'read',
+        expires: Date.now() + 5000,
+      });
+
+      const res = await fetch(signedReadUrl);
+      const body = await res.text();
+      assert.strictEqual(body, localFile.toString());
+    });
+
+    it('should create a signed read url with usableFrom', async () => {
+      const [signedReadUrl] = await file.getSignedUrl({
+        version: 'v4',
+        action: 'read',
+        usableFrom: Date.now() - 5000,
         expires: Date.now() + 5000,
       });
 
