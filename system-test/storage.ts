2038286621aec19e1ea391f5f232db180e548496
Sameena Shaffeeullah <shaffeeullah@google.com>
2021-06-04 20:26:49 +0000

    linted files

--- system-test/storage.ts
+++ system-test/storage.ts
@@ -3378,14 +3378,13 @@ describe('storage', () => {
         .file(fileName)
         .save('hello1', {resumable: false});
       await assert.rejects(
-        bucketWithVersioning
-            .file(fileName, {generation: 0})
-            .save('hello2'),
-            (err: ApiError) => {
-              assert.strictEqual(err.code, 412);
-              assert.strictEqual(err.errors![0].reason, 'conditionNotMet');
-              return true;
-            });
+        bucketWithVersioning.file(fileName, {generation: 0}).save('hello2'),
+        (err: ApiError) => {
+          assert.strictEqual(err.code, 412);
+          assert.strictEqual(err.errors![0].reason, 'conditionNotMet');
+          return true;
+        }
+      );
       await bucketWithVersioning
         .file(fileName)
         .save('hello3', {resumable: false});
