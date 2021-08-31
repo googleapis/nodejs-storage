ae8780ad51ec90a5661f3f0b90ecdb85e4b4a83d
Sameena Shaffeeullah <shaffeeullah@google.com>
2021-08-31 18:59:09 +0000

    built out tests further, still doesnt work

--- conformance-test/retryStrategy.ts
+++ conformance-test/retryStrategy.ts
@@ -16,41 +16,127 @@
 import {describe, it} from 'mocha';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as uuid from 'uuid';
+import pLimit = require('p-limit');
 
-import {Storage} from '../src/';
+require("conformance-test/libraryMethods.ts");
+import {Bucket, File, Iam, Notification, Storage} from '../src/';
 
-interface RetryTestCasesScenarioOne {
-  //probably should give it a more descriptive name. maybe instead of scenario we want it to be bucket, file, etc?
-  description: string;
-  bucket: string;
-  method: Function;
+interface RetryCase {
+  instructions: string[];
 }
 
-const testFile = fs.readFileSync(
-  path.join(__dirname, '../../conformance-test/test-data/v4SignedUrl.json'), //TODO change to new file
+interface Method {
+  name: String
+  resources: String[]
+}
+
+interface RetryTestCase {
+  id: number;
+  description: string;
+  retryCases: RetryCase[];
+  methods: Method[];
+  preconditionProvided: boolean;
+  expectSuccess: boolean;
+}
+
+interface MethodMap {
+  jsonApi: string;
+  nodejsStorageMethods: string[]
+}
+
+const testFile = fs.readFileSync( //TODO change to require
+  path.join(__dirname, '../../conformance-test/test-data/retryStrategyTestData.json'),
   'utf-8'
 );
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const testCases = JSON.parse(testFile);
-const retryTestCasesScenarioOne: RetryTestCasesScenarioOne[] =
-  testCases.retryTestCases;
+const testFileParsed = JSON.parse(testFile);
+const retryTestCases: RetryTestCase[] = testFileParsed.retryStrategyTests;
 
-const SERVICE_ACCOUNT = path.join(
-  __dirname,
-  '../../conformance-test/fixtures/signing-service-account.json' //TODO change to new service account
+const jsonToNodeApiMapping = fs.readFileSync( //TODO change to require
+  path.join(__dirname, '../../conformance-test/test-data/retryInvocationMap.json'),
+  'utf-8'
 );
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const methodMap: Map<String, String[]> = JSON.parse(jsonToNodeApiMapping);
 
-const storage = new Storage({keyFilename: SERVICE_ACCOUNT});
+const storage = new Storage(); //TODO: add apiEndpoint
 
 describe('retry conformance testing', () => {
-  describe('scenario one', () => {
-    //not sure this is how we want to do it
-    retryTestCasesScenarioOne.forEach(testCase => {
-      it(testCase.description, async () => {
-        const bucket = storage.bucket(testCase.bucket);
-        const result = testCase.method(bucket);
+  const TESTS_PREFIX = `storage-retry-tests-${shortUUID()}-`;
+  const RETENTION_DURATION_SECONDS = 10;
+  for (let testCaseIndex = 0; testCaseIndex < retryTestCases.length; testCaseIndex++){
+    const testCase = retryTestCases[testCaseIndex];
+    describe(`Scenario ${testCase.id}`, () => {
+      retryTestCases.retryCases.forEach(function(instructionSet:RetryCase) {
+        const instructions = instructionSet.instructions
+        //TODO set emulator based on instructions
+        testCase.methods.forEach(function (jsonMethod) {
+          const jsonMethodName = jsonMethod.name;
+          const jsonMethodResources = jsonMethod.resources
+          const functionList = methodMap.get(jsonMethodName);
+          functionList?.forEach(function (storageMethod) {
+
+            function generateName(bucketOrFile: string) {
+              return TESTS_PREFIX + storageMethod + bucketOrFile + shortUUID();
+            }
+
+            let bucket: Bucket;
+            let file: File;
+            let iam: Iam;
+            let notification: Notification
+            let storage: Storage
+            beforeEach(() => {
+              bucket = storage.bucket(generateName("bucket"));
+              file = bucket.file(generateName("file"));
+              notification = bucket.notification("notification");
+              // set preconditions if test says so
+
+            });
+
+            it(`${storageMethod}`, async () => { //if there are multiple cases, we're going to run into duplicate names here
+              const result = storageMethod(bucket);
+              // based on expectSuccess, make sure the right thing happens
+            });
+          
+            after(() => {
+              return Promise.all([deleteAllBucketsAsync()]);
+            });
+
+          });
+        });
       });
     });
-  });
+
+    async function deleteAllBucketsAsync() {
+      const [buckets] = await storage.getBuckets({prefix: TESTS_PREFIX});
+      const limit = pLimit(10);
+      await new Promise(resolve =>
+        setTimeout(resolve, RETENTION_DURATION_SECONDS * 1000)
+      );
+      return Promise.all(
+        buckets.map(bucket => limit(() => deleteBucketAsync(bucket)))
+      );
+    }
+
+    async function deleteBucketAsync(bucket: Bucket, options?: {}) {
+      // After files are deleted, eventual consistency may require a bit of a
+      // delay to ensure that the bucket recognizes that the files don't exist
+      // anymore.
+      const CONSISTENCY_DELAY_MS = 250;
+  
+      options = Object.assign({}, options, {
+        versions: true,
+      });
+  
+      await bucket.deleteFiles(options);
+      await new Promise(resolve => setTimeout(resolve, CONSISTENCY_DELAY_MS));
+      await bucket.delete();
+    }
+  }
 });
+
+function shortUUID() {
+  return uuid.v1().split('-').shift();
+}
+
