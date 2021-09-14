0f9cfafa712be27ca8bed4356002a3e79a21739d
Sameena Shaffeeullah <shaffeeullah@google.com>
2021-09-14 13:00:04 -0700

    Update libraryMethods.ts

--- conformance-test/libraryMethods.ts
+++ conformance-test/libraryMethods.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {O_APPEND} from 'constants';
 import {file} from 'tmp';
 import {Bucket, File, HmacKey, Iam, Notification, Storage} from '../src';
 import * as fs from 'fs';
