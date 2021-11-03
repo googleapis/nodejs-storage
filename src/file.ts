d82bffbf713bac63e0d3e8e2ecd0a50cfb60e83a
Denis DelGrosso <ddelgrosso@google.com>
2021-11-03 13:25:53 +0000

    fix: add scenario 3 conformance tests
    test: disable retries when creating write stream as part of upload.
    
    test: add scenario 3 tests
    
    update nodejs-common to 3.8.1
    
    test: disable autoRetry when calling createWriteStream from save
    
    linter fixes
    
    fix test
    
    fix unit tests by adding new string and additional guard case
    
    add delay to ensure docker container is started

--- src/file.ts
+++ src/file.ts
@@ -3756,6 +3756,9 @@ class File extends ServiceObject<File> {
     const returnValue = retry(
       async (bail: (err: Error) => void) => {
         await new Promise<void>((resolve, reject) => {
+          if (maxRetries === 0) {
+            this.storage.retryOptions.autoRetry = false;
+          }
           const writable = this.createWriteStream(options)
             .on('error', err => {
               if (
@@ -4081,7 +4084,13 @@ class File extends ServiceObject<File> {
  * that a callback is omitted.
  */
 promisifyAll(File, {
-  exclude: ['publicUrl', 'request', 'save', 'setEncryptionKey'],
+  exclude: [
+    'publicUrl',
+    'request',
+    'save',
+    'setEncryptionKey',
+    'shouldRetryBasedOnPreconditionAndIdempotencyStrat',
+  ],
 });
 
 /**
