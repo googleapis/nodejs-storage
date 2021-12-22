3d66c738bc22084a8eb09cbfaa59101757838de0
Sameena Shaffeeullah <shaffeeullah@google.com>
2021-12-22 20:06:13 +0000

    removed log

--- test/file.ts
+++ test/file.ts
@@ -132,7 +132,6 @@ const fakeOs = extend(true, {}, osCached);
 let resumableUploadOverride: any;
 function fakeResumableUpload() {
   return () => {
-    console.log('here fake resumable');
     return resumableUploadOverride || resumableUpload;
   };
 }
