3107ecada3d8d828363382748266292986da8860
Denis DelGrosso <ddelgrosso@google.com>
2022-01-27 18:41:26 +0000

    linter

--- conformance-test/globalHooks.ts
+++ conformance-test/globalHooks.ts
@@ -28,7 +28,9 @@ export async function mochaGlobalSetup(this: any) {
   this.suite._timeout = TIMEOUT_FOR_DOCKER_OPS;
   await getTestBenchDockerImage();
   await runTestBenchDockerImage();
-  await new Promise(resolve => setTimeout(resolve, TIME_TO_WAIT_FOR_CONTAINER_READY));
+  await new Promise(resolve =>
+    setTimeout(resolve, TIME_TO_WAIT_FOR_CONTAINER_READY)
+  );
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
