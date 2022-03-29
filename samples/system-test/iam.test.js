863e708c0b5089464126bfd3101f19e8557fe633
Daniel Bankhead <danielbankhead@google.com>
2022-03-29 13:52:08 -0700

    refactor: Slight refactor for clarity

--- samples/system-test/iam.test.js
+++ samples/system-test/iam.test.js
@@ -16,7 +16,7 @@
 
 const {Storage} = require('@google-cloud/storage');
 const {assert} = require('chai');
-const {before, after, it} = require('mocha');
+const {before, describe, after, it} = require('mocha');
 const cp = require('child_process');
 const uuid = require('uuid');
 
@@ -34,70 +34,72 @@ const description = 'Applies to objects matching a prefix';
 const expression =
   'resource.name.startsWith("projects/_/buckets/bucket-name/objects/prefix-a-")';
 
-before(async () => {
-  await bucket.create();
-  // UniformBucketLevelAccess must be enabled to add a conditional binding.
-  await bucket.setMetadata({
-    iamConfiguration: {
-      uniformBucketLevelAccess: {
-        enabled: true,
+describe('IAM', () => {
+  before(async () => {
+    await bucket.create();
+    // UniformBucketLevelAccess must be enabled to add a conditional binding.
+    await bucket.setMetadata({
+      iamConfiguration: {
+        uniformBucketLevelAccess: {
+          enabled: true,
+        },
       },
-    },
+    });
   });
-});
 
-after(async () => {
-  await bucket.delete().catch(console.error);
-});
+  after(async () => {
+    await bucket.delete();
+  });
 
-it('should add multiple members to a role on a bucket', async () => {
-  const output = execSync(
-    `node addBucketIamMember.js ${bucketName} ${roleName} "user:${userEmail}"`
-  );
-  assert.include(
-    output,
-    `Added the following member(s) with role ${roleName} to ${bucketName}:`
-  );
-  assert.match(output, new RegExp(`user:${userEmail}`));
-});
+  it('should add multiple members to a role on a bucket', async () => {
+    const output = execSync(
+      `node addBucketIamMember.js ${bucketName} ${roleName} "user:${userEmail}"`
+    );
+    assert.include(
+      output,
+      `Added the following member(s) with role ${roleName} to ${bucketName}:`
+    );
+    assert.match(output, new RegExp(`user:${userEmail}`));
+  });
 
-it('should add conditional binding to a bucket', async () => {
-  const output = execSync(
-    `node addBucketConditionalBinding.js ${bucketName} ${roleName} '${title}' '${description}' '${expression}' "user:${userEmail}"`
-  );
-  assert.include(
-    output,
-    `Added the following member(s) with role ${roleName} to ${bucketName}:`
-  );
-  assert.include(output, 'with condition:');
-  assert.include(output, `Title: ${title}`);
-  assert.include(output, `Description: ${description}`);
-  assert.include(output, `Expression: ${expression}`);
-});
+  it('should add conditional binding to a bucket', async () => {
+    const output = execSync(
+      `node addBucketConditionalBinding.js ${bucketName} ${roleName} '${title}' '${description}' '${expression}' "user:${userEmail}"`
+    );
+    assert.include(
+      output,
+      `Added the following member(s) with role ${roleName} to ${bucketName}:`
+    );
+    assert.include(output, 'with condition:');
+    assert.include(output, `Title: ${title}`);
+    assert.include(output, `Description: ${description}`);
+    assert.include(output, `Expression: ${expression}`);
+  });
 
-it('should list members of a role on a bucket', async () => {
-  const output = execSync(`node viewBucketIamMembers.js ${bucketName}`);
-  assert.match(output, new RegExp(`Bindings for bucket ${bucketName}:`));
-  assert.match(output, new RegExp(`Role: ${roleName}`));
-  assert.match(output, new RegExp('Members:'));
-  assert.match(output, new RegExp(`user:${userEmail}`));
-});
+  it('should list members of a role on a bucket', async () => {
+    const output = execSync(`node viewBucketIamMembers.js ${bucketName}`);
+    assert.match(output, new RegExp(`Bindings for bucket ${bucketName}:`));
+    assert.match(output, new RegExp(`Role: ${roleName}`));
+    assert.match(output, new RegExp('Members:'));
+    assert.match(output, new RegExp(`user:${userEmail}`));
+  });
 
-it('should remove multiple members from a role on a bucket', async () => {
-  const output = execSync(
-    `node removeBucketIamMember.js ${bucketName} ${roleName} "user:${userEmail}"`
-  );
-  assert.ok(
-    output.includes(
-      `Removed the following member(s) with role ${roleName} from ${bucketName}:`
-    )
-  );
-  assert.match(output, new RegExp(`user:${userEmail}`));
-});
+  it('should remove multiple members from a role on a bucket', async () => {
+    const output = execSync(
+      `node removeBucketIamMember.js ${bucketName} ${roleName} "user:${userEmail}"`
+    );
+    assert.ok(
+      output.includes(
+        `Removed the following member(s) with role ${roleName} from ${bucketName}:`
+      )
+    );
+    assert.match(output, new RegExp(`user:${userEmail}`));
+  });
 
-it('should remove conditional binding to a bucket', async () => {
-  const output = execSync(
-    `node removeBucketConditionalBinding.js ${bucketName} ${roleName} '${title}' '${description}' '${expression}'`
-  );
-  assert.include(output, 'Conditional Binding was removed');
+  it('should remove conditional binding to a bucket', async () => {
+    const output = execSync(
+      `node removeBucketConditionalBinding.js ${bucketName} ${roleName} '${title}' '${description}' '${expression}'`
+    );
+    assert.include(output, 'Conditional Binding was removed');
+  });
 });
