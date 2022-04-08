a24a2beff64ec851a06ef44104b1b09b3d4bc864
Sameena Shaffeeullah <shaffeeullah@google.com>
2022-04-08 18:13:04 +0000

    docs: mark function as private

--- src/file.ts
+++ src/file.ts
@@ -898,7 +898,8 @@ class File extends ServiceObject<File> {
    * A helper method for determining if a request should be retried based on preconditions.
    * This should only be used for methods where the idempotency is determined by
    * `ifGenerationMatch`
-   *
+   * @private
+   * 
    * A request should not be retried under the following conditions:
    * - if precondition option `ifGenerationMatch` is not set OR
    * - if `idempotencyStrategy` is set to `RetryNever`
