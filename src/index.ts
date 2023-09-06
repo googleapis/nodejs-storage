1f39462efe5a6db6945a9c0b4111bad9a03fd5de
Daniel Bankhead <danielbankhead@google.com>
2023-09-06 11:27:06 -0700

    refactor: Export `ApiError` only

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,7 @@
  * region_tag:storage_quickstart
  * Full quickstart example:
  */
-export * from './nodejs-common';
+export {ApiError} from './nodejs-common';
 export {
   AclMetadata,
   AccessControlObject,
