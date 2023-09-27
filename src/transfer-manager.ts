62d056efb18588fb820683b071a9f1bea64901cf
Denis DelGrosso <ddelgrosso@google.com>
2023-09-27 19:15:10 +0000

    revert parse function

--- src/transfer-manager.ts
+++ src/transfer-manager.ts
@@ -259,11 +259,7 @@ class XMLMultiPartUploadHelper implements MultiPartUploadHelper {
         if (res.data && res.data.error) {
           throw res.data.error;
         }
-        const parsedXML = this.xmlParser.parse<{
-          InitiateMultipartUploadResult: {
-            UploadId: string;
-          };
-        }>(res.data);
+        const parsedXML = this.xmlParser.parse(res.data);
         this.uploadId = parsedXML.InitiateMultipartUploadResult.UploadId;
       } catch (e) {
         this.#handleErrorResponse(e as Error, bail);
