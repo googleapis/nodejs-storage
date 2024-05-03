3f4667bea9422ddbf51fe6f41c1d3dbdcf7a12a5
Denis DelGrosso <ddelgrosso@google.com>
2024-05-03 14:44:57 +0000

    feat: add ability to enable hierarchical namespace on buckets

--- src/storage.ts
+++ src/storage.ts
@@ -124,6 +124,9 @@ export interface CreateBucketRequest {
   customPlacementConfig?: CustomPlacementConfig;
   dra?: boolean;
   enableObjectRetention?: boolean;
+  hierarchicalNamespace?: {
+    enabled?: boolean;
+  };
   location?: string;
   multiRegional?: boolean;
   nearline?: boolean;
@@ -868,6 +871,7 @@ export class Storage extends Service {
    * @property {boolean} [dra=false] Specify the storage class as Durable Reduced
    *     Availability.
    * @property {boolean} [enableObjectRetention=false] Specifiy whether or not object retention should be enabled on this bucket.
+   * @property {object} [hierarchicalNamespace.enabled=false] Specify whether or not to enable hierarchical namespace on this bucket.
    * @property {string} [location] Specify the bucket's location. If specifying
    *     a dual-region, the `customPlacementConfig` property should be set in conjunction.
    *     For more information, see {@link https://cloud.google.com/storage/docs/locations| Bucket Locations}.
