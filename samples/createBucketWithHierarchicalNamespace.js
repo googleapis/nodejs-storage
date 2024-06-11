7e14e9b08345f6a1a7b3f4ddfd8a06056261c95e
Denis DelGrosso <ddelgrosso@google.com>
2024-06-11 12:38:16 +0000

    fix fields

--- samples/createBucketWithHierarchicalNamespace.js
+++ samples/createBucketWithHierarchicalNamespace.js
@@ -40,7 +40,9 @@ function main(bucketName = 'my-bucket') {
   async function createBucketWithHierarchicalNamespace() {
     const [bucket] = await storage.createBucket(bucketName, {
       iamConfiguration: {
-        uniformBucketLevelAccess: true,
+        uniformBucketLevelAccess: {
+          enabled: true,
+        },
       },
       hierarchicalNamespace: {
         enabled: true,
