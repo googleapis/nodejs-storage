53b69b57062b5ea8687e8c5758ce8a0bf4787e6c
Sofia Leon <sofialeon@google.com>
2026-03-03 13:28:30 -0800

    build: update README for deprecation notice

--- owlbot.py
+++ owlbot.py
@@ -1,43 +1,17 @@
-# Copyright 2019 Google LLC
+# Copyright 2022 Google LLC
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the License);
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import synthtool as s
-import synthtool.gcp as gcp
 import synthtool.languages.node as node
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-
-common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library(source_location='build/src')
-s.copy(templates, excludes=['.jsdoc.js',
-                            '.github/release-please.yml',
-                            '.github/sync-repo-settings.yaml',
-                            '.github/workflows/ci.yaml',
-                            '.prettierrc.js',
-                            '.mocharc.js',
-                            '.kokoro/continuous/node14/system-test.cfg',
-                            '.kokoro/presubmit/node14/system-test.cfg',
-                            '.kokoro/release/publish.cfg',
-                            '.kokoro/system-test.sh',
-                            '.kokoro/samples-test.sh',
-                            ])
-
-# Create .config directory under $HOME to get around permissions issues
-# with resumable upload.
-s.replace(
-    ".circleci/config.yml",
-    "command: npm run system-test",
-    "command: mkdir $HOME/.config && npm run system-test")
-node.fix()
+node.owlbot_main(templates_excludes=[
+'README.md'
+])
